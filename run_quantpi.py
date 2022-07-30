c9a9beeb0c60ea9b362c2027dd44df13c830e7e8
alienzj <alienchuj@gmail.com>
2022-07-30 14:19:19 +0800

    package it

---
+++ run_quantpi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+from metapi.corer import main
+main()
---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup
+
+exec(open("quantpi/__about__.py").read())
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+packages = ["quantpi"]
+package_data = {
+    "quantpi": [
+        "quantpi/config/*.yaml",
+        "quantpi/envs/*.yaml",
+        "quantpi/snakefiles/*.smk",
+        "quantpi/rules/*.smk",
+        "quantpi/wrappers/*.py",
+        "quantpi/*.py",
+    ]
+}
+data_files = [(".", ["LICENSE", "README.md"])]
+
+entry_points = {"console_scripts": ["metapi=quantpi.corer:main"]}
+
+requires = [
+    req.strip()
+    for req in open("requirements.txt", "r").readlines()
+    if not req.startswith("#")
+]
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
+setup(
+    name="quantpi",
+    version=__version__,
+    author=__author__,
+    author_email="alienchuj@gmail.com",
+    url="https://github.com/ohmeta/quantpi",
+    description="microbiome profiling pipeline",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    entry_points=entry_points,
+    packages=packages,
+    package_data=package_data,
+    data_files=data_files,
+    include_package_data=True,
+    install_requires=requires,
+    license="GPLv3+",
+    classifiers=classifiers,
+)
